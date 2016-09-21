b61db67450aefc2c1163d6f1893587c663fb4e86
Mark Shevchenko <m.shevchenko@taxys.ru>
2016-09-21 16:01:01 +0300

    Написал каркас приложения.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Network.Wai
+import Network.HTTP.Types (status200, hContentType)
+import Network.Wai.Handler.Warp (run)
+
+application :: Application
+application _ respond = respond $
+  responseLBS status200
+              [(hContentType, "text/plain")]
+              "Hello, world"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Serving..."
+  run 8000 application
