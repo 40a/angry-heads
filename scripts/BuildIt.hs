0350ff417ea7eaebcf860d7704418bba22476b13
Aleksey Pirogov <astynax@users.noreply.github.com>
2016-09-21 23:09:01 +0300

    добавляет build tool для кроссплатформенной сборки

---
+++ scripts/BuildIt.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+
+-- | Cross-platform Make replacement :)
+
+import Turtle hiding (within)
+
+
+data Target = Client | Server | All
+
+
+within :: Turtle.FilePath -> IO a -> IO a
+within targetDir body = do
+    oldDir <- pwd
+    cd targetDir
+    result <- body
+    cd oldDir
+    return result
+
+
+buildClient :: IO ExitCode
+buildClient = do
+    echo "Building: client..................."
+    within "client"
+        $ shell "elm-make src/Main.elm --output ../static/app.js" empty
+
+
+buildServer :: IO ExitCode
+buildServer = do
+    echo "Building: server..................."
+    shell "stack build angry-heads:exe:server" empty
+
+
+main :: IO ()
+main =
+    options "BuildIt, an awesome build tool!"
+    ( subcommand "client" "Build a client" (pure Client)
+      <|> subcommand "server" "Build a server" (pure Server)
+      <|> pure All
+    )
+    >>= \case
+        Client -> buildClient
+        Server -> buildServer
+        _ -> buildClient .&&. buildServer
+    >>= exit
