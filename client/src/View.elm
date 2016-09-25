ce57975dcc617a215bd311bc1b8bc5c1a910153c
Aleksey Pirogov <astynax@users.noreply.github.com>
2016-09-25 16:35:40 +0300

    добавляет болванку UI (на базе Twitter Bootstrap)

--- client/src/View.elm
+++ client/src/View.elm
@@ -1,8 +1,58 @@
 module View exposing (view)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 import Model exposing (..)
 
+
 view : Model -> Html Msg
-view m = h1 [] [text m]
+view m =
+  div []
+    [ nav
+    , page m ]
+
+
+nav : Html a
+nav =
+  node "nav" [ class "navbar navbar-inverse navbar-fixed-top" ]
+    <: container
+       [ div [ class "navbar-header" ]
+           [ node "button" [ type' "button"
+                           , class "navbar-toggle collapsed"
+                           , attribute "data-toggle" "collapse"
+                           , attribute "data-target" "#navbar"
+                           , attribute "aria-expanded" "false"
+                           , attribute "aria-controls" "navbar" ]
+               [ span [ class "sr-only" ] [ text "Toggle navigation" ]
+               -- "hamburger" menu
+               , span [ class "icon-bar"] []
+               , span [ class "icon-bar"] []
+               , span [ class "icon-bar"] []
+               ]
+           , a [ class "navbar-brand", href "#" ] <: text "Angry Heads"
+           ]
+       , div [ id "navbar"
+             , class "collapse navbar-collapse" ]
+           <: ul [ class "nav navbar-nav" ]
+              [ li [ class "active" ] <: a [ href "#" ] <: text "Home"
+              , li [] <: a [ href "#about" ] <: text "About"
+              , li [] <: a [ href "#contact" ] <: text "Contact"
+              ]
+       ]
+
+
+page : Model -> Html Msg
+page m =
+  container
+    <: h1 []
+       <: text m
+
+-- Helpers
+
+infixr 9 <:
+(<:) : (List a -> b) -> a -> b
+(<:) n1 n2 = n1 <| [n2]
+
+container : List (Html a) -> Html a
+container = div [ class "container" ]
