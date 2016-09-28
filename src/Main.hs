279e3a8b8f3b71b3acf14532c92aa48f0307d94e
Mark Shevchenko <m.shevchenko@taxys.ru>
2016-09-28 10:44:46 +0300

    Отправляю запрос на hh.ru и получаю ответ, пока только статус. Следующая задача: парсить пришедший JSON и складывать пришедише токены в cookie.

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
+import Control.Monad.Trans (lift)
+import Data.Aeson (Value)
+import Data.Text.Lazy (pack)
+import Data.ByteString.Char8 (pack)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.RequestLogger (logStdout)
+import Network.HTTP.Conduit
+import Network.HTTP.Simple
+import System.Environment (getEnv)
 import Web.Scotty (ScottyM, scottyApp, get, text, param)
 
 import Options
 import Static
-
 
 app :: ScottyM ()
 app = do
@@ -16,7 +23,15 @@ app = do
 
     get "/oauth/hh" $ do
         code <- param "code"
-        text code
+        clientId <- lift $ getEnv "HeadHunterOAuthClientID"
+        clientSecret <- lift $ getEnv "HeadHunterOAuthClientSecret"
+        let request = urlEncodedBody [("grant_type", "authorization_code"),
+                                      ("client_id", Data.ByteString.Char8.pack clientId),
+                                      ("client_secret", Data.ByteString.Char8.pack clientSecret),
+                                      ("code", Data.ByteString.Char8.pack code)]
+                    $ parseRequest_ "POST https://hh.ru/oauth/token"
+        (response :: Response Value) <- httpJSON request
+        text $ Data.Text.Lazy.pack $ show (getResponseStatus response) ++ code
 
 main :: IO ()
 main = do
