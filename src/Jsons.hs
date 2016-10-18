19f956ca095bec14686785d48370739522ae4ed8
Aleksey Pirogov <astynax@users.noreply.github.com>
2016-10-18 16:53:51 +0300

    Слегка переформатирует код

--- src/Jsons.hs
+++ src/Jsons.hs
@@ -8,18 +8,21 @@ import Control.Applicative ((<|>))
 
 data AccessToken = AccessToken { access_token :: Text
                                , token_type :: Text
-                               , expires_in :: Int                                               
+                               , expires_in :: Int
                                , refresh_token :: Text
                                } deriving (Generic, Show)
+
+data Error = Error { error :: Text
+                   , error_description :: Text
+                   } deriving (Generic, Show)
+
+data HHResult = HHSuccess AccessToken
+              | HHError Error
 
 instance ToJSON AccessToken where
     toEncoding = genericToEncoding defaultOptions
 
 instance FromJSON AccessToken where { }
-
-data Error = Error { error :: Text
-                   , error_description :: Text
-                   } deriving (Generic, Show)
 
 instance ToJSON Error where
     toEncoding = genericToEncoding defaultOptions
